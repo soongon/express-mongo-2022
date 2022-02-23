1ac2c1dfd3b1b3f7c6c60495cebba01755b5659c
soongon <soongon@hucloud.co.kr>
2022-02-23 17:10:48 +0900

    initial commit

--- app.js
+++ app.js
@@ -34,16 +34,39 @@ app.get('/employees/:empId', (req, res) => {
 // 직원 등록
 app.post('/employees', (req, res) => {
   const emp = req.body;
-  console.log(emp);
-  res.json(emp);
+  Employee.create(emp, (err, saved) => {
+    if (err) { 
+      res.send(err);
+    } else {
+      res.send(emp);
+    }
+  });
 });
 // 직원 수정
 app.put('/employees', (req, res) => {
+  const emp = req.body;
+  Employee.findByIdAndUpdate(emp._id, {
+    name: emp.name,
+    team: emp.team,
+    salary: emp.salary
+  }, (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(emp);
+    }
+  });
 
 });
 // 직원 삭제
 app.delete('/employees/:empId', (req, res) => {
-
+  Employee.findByIdAndDelete(parseInt(req.params.empId), (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('delete ok..');
+    }
+  });
 });
 
 
